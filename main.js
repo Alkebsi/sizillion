3f94bc94543ffb999b433b632dfd3b54db242c08
Mohammed Alkebsi <moha.alkebsi@gmail.com>
2022-06-07 13:57:30 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//calling the top loading line
+let topLine =
+document.getElementsByClassName("topLine")[0];
+
+//this is the start function for the site(e.i. Activated when the page load)
+function Start()
+{
+	topLine.style.display = "none";
+}
